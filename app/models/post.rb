c51d1604b329bfcaa7449890e031e59c9612be8e
RadoszSzymon <radoszszymon@gmail.com>
2016-07-15 12:36:57 +0200

    deploy

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ActiveRecord::Base
+	has_many :comments, dependent: :destroy
+	validates :title, presence: true, length: {minimum: 5}
+	validates :body,  presence: true
+
+	has_attached_file :image, styles: { medium: "300x300>" }
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+get 'posts/main'
+
+  devise_for :users, controllers: { registrations: "registrations"}
+  resources :posts do
+  	resources :comments
+  end
+
+  root "posts#index"
+
+  get '/about', to: 'pages#about'
+
+end
