bafcc0fdcc3fa0dfa2d9de59116c8e04f0bf80a0
RadoszSzymon <radoszszymon@gmail.com>
2016-07-15 12:47:19 +0200

    resize images

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ActiveRecord::Base
 	validates :title, presence: true, length: {minimum: 5}
 	validates :body,  presence: true
 
-	has_attached_file :image, styles: { medium: "300x300>" }
+	has_attached_file :image, styles: { medium: "200x200>" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
