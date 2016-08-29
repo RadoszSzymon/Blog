774b8eb1b1dffb58beecc17e0489dbba8e0aacfe
RadoszSzymon <radoszszymon@gmail.com>
2016-08-29 21:07:32 +0200

    added random posts to sidebar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,9 @@ class PostsController < ApplicationController
 
 	def show
 		@post = Post.find(params[:id])
+		@random_post = Post.where.not(id: @post).order("RANDOM()").first
+    	@random_post2 = Post.where.not(id: @post).order("RANDOM()").second
+		@random_post3 = Post.where.not(id: @post).order("RANDOM()").third
 	end
 
 	def create
